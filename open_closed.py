1d36eb2ed23ecae735847dd1052926facb741f74
Menna-Ali56 <mennaalist@gmail.com>
2024-12-22 14:40:49 +0200

    Add files via upload

--- open_closed.py
+++ open_closed.py
@@ -1,40 +1,38 @@
-#open /closed principle (OCP)
-#open -->foe extention(can add any method without error and don't edit any function)
-#closed --> for modify (edit the code coding before but only in new class 
-from abc import ABC, abstractmethod
+#this code is violate open_closed
+class TaxCalculator:
+    def __init__(self, country, amount):
+        self.country = country
+        self.amount = amount
+    
+    def calculate_tax(self):
+        if self.country == "USA":
+            return self.amount * 0.1  # 10% tax in USA
+        elif self.country == "India":
+            return self.amount * 0.2  # 20% tax in India
+        elif self.country == "UK":
+            return self.amount * 0.15  # 15% tax in UK
+        else:
+            return 
+        
+        
+        
+#this code isnot violate open_closed
+class TaxCalculator:
+    def __init__(self, amount, tax_strategy):
+        self.amount = amount
+        self.tax_strategy = tax_strategy
+    
+    def calculate_tax(self):
+        return self.tax_strategy.calculate_tax(self.amount)
 
-class DiscountStrategy(ABC):
-    @abstractmethod
-    def apply_discount(self, price):
-        pass
+class USATaxStrategy:
+    def calculate_tax(self, amount):
+        return amount * 0.1
 
+class IndiaTaxStrategy:
+    def calculate_tax(self, amount):
+        return amount * 0.2
 
-class NoDiscount(DiscountStrategy):
-    def apply_discount(self, price):
-        return price
-
-
-class SeasonalDiscount(DiscountStrategy):
-    def apply_discount(self, price):
-        return price * 0.8  
-
-
-class Product:
-    def __init__(self, name, price, discount_strategy):
-        self.name = name
-        self.price = price
-        self.discount_strategy = discount_strategy
-
-    def get_price(self):
-        return self.discount_strategy.apply_discount(self.price)
-
-
-
-product1=Product("x", 200, NoDiscount())
-print(product1.get_price()) #output 200
-product2=Product("y", 200, SeasonalDiscount())
-print(product2.get_price()) #output 160
-
-
-
-
+class UKTaxStrategy:
+    def calculate_tax(self, amount):
+        return amount * 0.15
